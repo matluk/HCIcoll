c7cffcc9821010ef80d301ff3d942d361ddadb13
amilin00 <amilin00@fesb.hr>
2020-02-17 20:05:58 +0100

    dodani botuni za natrag

--- restApp/src/pages/rezervacije.js
+++ restApp/src/pages/rezervacije.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout'
 import calendarIcon from '../images/svg_pics/calendar_today-24px.svg'
 import watchIcon from '../images/svg_pics/access_time-24px_gray.svg'
 import bookIcon from '../images/svg_pics/menu_book-24px_page_gray.svg'
+import { Link } from 'gatsby'
 
 export default function rezervacije() {
     return (
@@ -27,8 +28,8 @@ export default function rezervacije() {
                     <input type='text' placeholder='Unesite tekst' />
                     <br />
                     <br />
-                    <button>Back</button>
-                    <button>Rezerviraj</button>
+                    <button><Link to="/" style={{textDecoration: "none", color: "black"}}>Back</Link></button>
+                    <button><Link to="/" style={{textDecoration: "none", color: "black"}}>Rezerviraj</Link></button>
                 </form>
             </div>
         </Layout>
